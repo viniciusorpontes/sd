1a259da02c5e6e1ffb2268b4025d995c1543e835
Vinícius Pontes <vinicius.pontes@alunos.fibbauru.br>
2023-03-06 02:07:26 -0300

    Criando aplicação e configura banco.

---
+++ src/main/java/br/com/sd/SdApplication.java
@@ -0,0 +1,13 @@
+package br.com.sd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SdApplication.class, args);
+    }
+
+}
